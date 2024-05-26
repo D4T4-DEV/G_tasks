4dfd81d54a1c55db612f8d25195453e0db059383
David Alejandro Trejo Avila <d4t4_dev@hotmail.com>
2024-05-26 14:46:50 -0600

    Creacion de una nueva vista

--- Controllers/Routes/routes.js
+++ Controllers/Routes/routes.js
@@ -12,18 +12,27 @@ const loginView = require('./loginView');
 // Ruta de la pantalla principal
 const pPrincipalView = require('./indexView');
 
+// Ruta de la pantalla de "myCreatedTask"
+const pMyCreatedTask = require('./createdTaskView');
 
 // RUTAS para usar en AJAX
-const getDataCounterIndex = require('./solicitudesAJAX/getDataCounterIndex')
+const getDataCounterIndex = require('./solicitudesAJAX/getDataCounterIndex');
+const getDataNotifications = require('./solicitudesAJAX/getStatesNotification');
 
 
 // Confuguracion de las rutas AJAX
 router.use('/get-counter-data', getDataCounterIndex);
+router.use('/get-state-notif', getDataNotifications);
 
 
 // Configuracion login
 // router.use('/', loginView);
-router.use('/', pPrincipalView);
+
+// Configuracion de la pantalla principal
+router.use('/principal', pPrincipalView);
+
+// Configuracion de la pantalla de myCreatedTask
+router.use('/created-tasks', pMyCreatedTask);
 
 
 
--- Controllers/Routes/solicitudesAJAX/getStatesNotification.js
+++ Controllers/Routes/solicitudesAJAX/getStatesNotification.js
@@ -5,19 +5,11 @@
 
 const express = require('express');
 const router = express.Router();
-
-var numCreate = 0;
-var numNews = 0;
-var numPending = 0;
-var numComplet = 0;
-
+var stateNotification = 0;
 // Ruta de respuesta AJAX de la vista 'index'
 router.get('/', (req, res) => {
     res.json({
-        numCreate: numCreate++, // Cambia esto es de prueba
-        numNews: numNews,
-        numPending: numPending,
-        numComplet: numComplet
+        notification: Math.floor(Math.random() * 3) + 1 // Cambiar esto, solo es prueba uwu
     });
 });
 
